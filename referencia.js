028609bfd9b0034ac3e00273bab71081e80dda12
Lucas Espinillo <lucasjoel2187@gmail.com>
2023-01-30 18:35:51 -0300

    proyecto final

---
+++ referencia.js
@@ -0,0 +1,18 @@
+function referencia1 () {
+    document.write("Diego lopez,1130526588")
+
+}
+
+function referencia2 () {
+    document.write("Gastón Diaz, 1165259656")
+
+}
+
+function referencia3 () {
+    document.write("Pedro Lazarte, 1145895692")
+
+}
+function referencia4 () {
+    document.write("Juan Medrano, 1154857589")
+
+}
